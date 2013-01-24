53e1ef03a6a3f46e4f07c8ff0abac34fae0f9e01
Haruka Yoshihara <yoshihara@clear-code.com>
2013-01-24 17:54:33 +0900

    Move client/client.rb to client.rb because of bad name

--- lib/groonga/client.rb
+++ lib/groonga/client.rb
@@ -16,5 +16,123 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-require "groonga/client/client"
-require "groonga/client/version"
+require "groonga/client/command"
+require "groonga/client/protocol/gqtp"
+require "groonga/client/protocol/http"
+
+module Groonga
+  class Client
+    class << self
+      def open(options={}, &block)
+        client = new(options)
+        if block_given?
+          yield(client)
+          client.close
+        else
+          client
+        end
+      end
+    end
+
+    attr_reader :protocol, :real_client
+
+    def initialize(options)
+      @protocol = options[:protocol] || :gqtp
+      options.delete(:protocol)
+
+      @real_client = nil
+      if @protocol == :gqtp
+        options[:connection] ||= :synchronous
+        @real_client = Groonga::Client::RealClient::GQTP.new(options)
+      else
+        @real_client = Groonga::Client::RealClient::HTTP.new(options)
+      end
+    end
+
+    def close
+    end
+
+    def cache_limit(parameters)
+    end
+
+    def check(parameters)
+    end
+
+    def clearlock(parameters)
+    end
+
+    def column_create(parameters)
+    end
+
+    def column_list(parameters)
+    end
+
+    def column_remove(parameters)
+    end
+
+    def column_rename(parameters)
+    end
+
+    def define_selector(parameters)
+    end
+
+    def defrag(parameters)
+    end
+
+    def delete(parameters)
+    end
+
+    def dump(parameters)
+    end
+
+    def load(parameters)
+    end
+
+    def log_level(parameters)
+    end
+
+    def log_put(parameters)
+    end
+
+    def log_reopen
+    end
+
+    def quit
+    end
+
+    def register(parameters)
+    end
+
+    def select(parameters)
+    end
+
+    def shutdown
+    end
+
+    def status
+      execute_command("status")
+    end
+
+    def table_create(parameters)
+    end
+
+    def table_list
+      execute_command("table_list")
+    end
+
+    def table_remove(parameters)
+    end
+
+    def table_rename(parameters)
+    end
+
+    def truncate(parameters)
+    end
+
+    private
+    def execute_command(command_name, parameters={})
+      command = Groonga::Command::Base.new(command_name, parameters)
+      Client::Command.new(command).execute(@real_client, @protocol)
+    end
+  end
+end
