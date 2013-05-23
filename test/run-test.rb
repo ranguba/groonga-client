3ddfb10a52dedc77ad15346cdebbd43342f50fbd
Kouhei Sutou <kou@clear-code.com>
2013-05-23 17:40:32 +0900

    Use Pathname

--- test/run-test.rb
+++ test/run-test.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 #
 # Copyright (C) 2013  Haruka Yoshihara <yoshihara@clear-code.com>
+# Copyright (C) 2013  Kouhei Sutou <kou@clear-code.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -18,20 +19,22 @@
 
 $VERBOSE = true
 
-base_dir = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+require "pathname"
 
-gqtp_base_dir = File.expand_path(File.join(base_dir, "..", "gqtp"))
-gqtp_lib_dir = File.join(gqtp_base_dir, "lib")
-$LOAD_PATH.unshift(gqtp_lib_dir)
+base_dir = Pathname.new(__FILE__).dirname.parent.expand_path
 
-lib_dir = File.join(base_dir, "lib")
-test_dir = File.join(base_dir, "test")
+gqtp_base_dir = base_dir.parent + "gqtp"
+gqtp_lib_dir = gqtp_base_dir + "lib"
+$LOAD_PATH.unshift(gqtp_lib_dir.to_s)
 
-$LOAD_PATH.unshift(lib_dir)
+lib_dir = base_dir + "lib"
+test_dir = base_dir + "test"
+
+$LOAD_PATH.unshift(lib_dir.to_s)
 
 require "test-unit"
 require "test/unit/notify"
 
 Thread.abort_on_exception = true
 
-exit Test::Unit::AutoRunner.run(true, test_dir)
+exit Test::Unit::AutoRunner.run(true, test_dir.to_s)
