8fe44f89ee8235cefb404abd3174842e62851b81
Kosuke Asami <tfortress58@gmail.com>
2013-06-29 01:37:15 +0900

    Provide accessors for Response::Select class

--- lib/groonga/client/response/select.rb
+++ lib/groonga/client/response/select.rb
@@ -5,6 +5,54 @@ module Groonga
     module Response
       class Select < Base
         Response.register("select", self)
+
+        attr_accessor :records, :total_records
+        attr_accessor :drilldowns, :total_drilldowns
+
+        def initialize(header, body)
+          super(header, parse_body(body))
+        end
+
+        private
+        def parse_body(body)
+          @total_records, @records = parse_match_items(body.first)
+          @total_drilldowns, @drilldowns = parse_drilldowns(body.last)
+          body
+        end
+
+        def parse_match_items(match_items)
+          total_records = match_items.first.first
+          properties = match_items[1]
+          infos = match_items[2..-1]
+          records = infos.collect do |info|
+            record = Record.new
+            properties.each_with_index do |(name, _), i|
+              record[name] = info[i]
+            end
+            record
+          end if infos
+          [total_records, records]
+        end
+
+        def parse_drilldowns(drilldowns)
+          total_drilldowns = drilldowns.first.first
+          properties = drilldowns[1]
+          infos = drilldowns[2..-1]
+          records = infos.collect do |info|
+            record = Drilldown.new
+            properties.each_with_index do |(name, _), i|
+              record[name] = info[i]
+            end
+            record
+          end if infos
+          [total_drilldowns, records]
+        end
+
+        class Record < Hash
+        end
+
+        class Drilldown < Hash
+        end
       end
     end
   end
--- test/command/test-select.rb
+++ test/command/test-select.rb
@@ -12,5 +12,50 @@ class TestCommandSelect < Test::Unit::TestCase
     end
     assert_equal(body, client.select(:table => :Tests).body)
   end
+
+  def test_response
+    client = Groonga::Client.open(:protocol => :http)
+    header = [0,1372430096.70991,0.000522851943969727]
+    body = [[[6],[["_id","UInt32"],["country","Country"]],[1,"japan"],[2,"brazil"],[3,"japan"],[4,"usa"],[5,"japan"],[6,"usa"]],
+      [[3],[["_key","ShortText"],["_nsubrecs","Int32"]],["japan",3],["brazil",1],["usa",2]]]
+    response = Groonga::Client::Response::Select.new(header, body)
+    mock(client).execute_command("select", :table => :Test) do
+      response
+    end
+    select = client.select(:table => :Test)
+
+    assert_equal(6, select.total_records)
+    expected_records = [
+      {"_id"=>1, "country"=>"japan"},
+      {"_id"=>2, "country"=>"brazil"},
+      {"_id"=>3, "country"=>"japan"},
+      {"_id"=>4, "country"=>"usa"},
+      {"_id"=>5, "country"=>"japan"},
+      {"_id"=>6, "country"=>"usa"},
+    ]
+    assert_equal(expected_records, select.records)
+
+    assert_equal(3, select.total_drilldowns)
+    expected_drilldowns = [
+      {"_key"=>"japan", "_nsubrecs"=>3},
+      {"_key"=>"brazil", "_nsubrecs"=>1},
+      {"_key"=>"usa", "_nsubrecs"=>2},
+    ]
+    assert_equal(expected_drilldowns, select.drilldowns)
+  end
+
+  def test_response_limit_zero
+    client = Groonga::Client.open(:protocol => :http)
+    header = [0,1372430096.70991,0.000522851943969727]
+    body = [[[6],[["_id","UInt32"],["country","Country"]]]]
+    response = Groonga::Client::Response::Select.new(header, body)
+    mock(client).execute_command("select", :table => :Test) do
+      response
+    end
+    select = client.select(:table => :Test)
+
+    assert_equal(6, select.total_records)
+    assert_equal([], select.records)
+  end
 end
 
