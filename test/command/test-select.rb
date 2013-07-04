3e371e035586761675af39ed366fa76a1d252db1
Kosuke Asami <tfortress58@gmail.com>
2013-07-04 14:29:58 +0900

    test: use instance variables instead of local variables
    Because:
    these are commonly used in TestCommandSelect

--- test/command/test-select.rb
+++ test/command/test-select.rb
@@ -8,13 +8,12 @@ class TestCommandSelect < Test::Unit::TestCase
 
   def setup
     @header = [0,1372430096.70991,0.000522851943969727]
+    @body = [[[1], [["_id", "UInt32"]], [1]]]
   end
 
   def test_request
     client = open_client
-    header = @header
-    body = [[[1], [["_id", "UInt32"]], [1]]]
-    response = Groonga::Client::Response::Select.new(header, body)
+    response = Groonga::Client::Response::Select.new(@header, @body)
     mock(client).execute_command("select", :table => :TestTable) do
       response
     end
@@ -23,9 +22,7 @@ class TestCommandSelect < Test::Unit::TestCase
 
   def test_response
     client = open_client
-    header = @header
-    body = [[[1], [["_id", "UInt32"]], [1]]]
-    stub(client.connection).send.with_any_args.yields([header, body].to_json) do
+    stub(client.connection).send.with_any_args.yields([@header, @body].to_json) do
       request = Object.new
       stub(request).wait do
         true
