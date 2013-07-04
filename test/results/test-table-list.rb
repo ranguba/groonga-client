65fa2bb1819bfb5b5af623e4d1cb955861af4941
Kosuke Asami <tfortress58@gmail.com>
2013-07-04 17:06:26 +0900

    test: move a parsed results object to a setup method

--- test/results/test-table-list.rb
+++ test/results/test-table-list.rb
@@ -7,9 +7,6 @@ class TestResultsTableList < Test::Unit::TestCase
 
   def setup
     @header = [0,1372430096.70991,0.000522851943969727]
-  end
-
-  def test_results
     header = @header
     body = [[["id","UInt32"],["name","ShortText"],["path","ShortText"],["flags","ShortText"],["domain","ShortText"],["range","ShortText"],["default_tokenizer","ShortText"],["normalizer","ShortText"]],
       [257,"Ages","/tmp/test.db.0000101","TABLE_DAT_KEY|PERSISTENT","UInt32",nil,nil,nil],
@@ -17,13 +14,16 @@ class TestResultsTableList < Test::Unit::TestCase
       [258,"Logs","/tmp/test.db.0000102","TABLE_NO_KEY|PERSISTENT",nil,nil,nil,nil]]
     response = Groonga::Client::Response::TableList.new(header, body)
 
-    table_list = response
-    assert_equal(3, table_list.size)
-    table_list.each do |table|
+    @table_list = response
+  end
+
+  def test_results
+    assert_equal(3, @table_list.size)
+    @table_list.each do |table|
       assert_equal(Groonga::Client::Response::TableList::Table, table.class)
     end
 
-    table = table_list[1]
+    table = @table_list[1]
     assert_equal(256, table.id)
     assert_equal("Lexicon", table.name)
     assert_equal("/tmp/test.db.0000100", table.path)
