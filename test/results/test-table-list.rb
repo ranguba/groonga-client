35380cd6179b8f7d28a9357f3ef4fc61caca2b4c
Kosuke Asami <tfortress58@gmail.com>
2013-07-04 14:22:40 +0900

    test: extract tests from test-table-list that are related to parsed results

--- test/results/test-table-list.rb
+++ test/results/test-table-list.rb
@@ -1,28 +1,15 @@
 require "test/unit/rr"
 
-require "command/helper"
+require "results/helper"
 
-class TestCommandTableList < Test::Unit::TestCase
-  include TestCommandHelper
+class TestResultsTableList < Test::Unit::TestCase
+  include TestResultsHelper
 
   def setup
     @header = [0,1372430096.70991,0.000522851943969727]
   end
 
-  def test_request
-    client = open_client
-    header = @header
-    body = [[["id","UInt32"],["name","ShortText"],["path","ShortText"],["flags","ShortText"],["domain","ShortText"],["range","ShortText"],["default_tokenizer","ShortText"],["normalizer","ShortText"]],
-      [256,"Test","/tmp/test.db.0000100","TABLE_HASH_KEY|PERSISTENT",nil,nil,nil,nil]]
-    response = Groonga::Client::Response::TableList.new(header, body)
-    mock(client).execute_command("table_list", {}) do
-      response
-    end
-
-    assert_equal(header, client.table_list.header)
-  end
-
-  def test_response
+  def test_results
     client = open_client
     header = @header
     body = [[["id","UInt32"],["name","ShortText"],["path","ShortText"],["flags","ShortText"],["domain","ShortText"],["range","ShortText"],["default_tokenizer","ShortText"],["normalizer","ShortText"]],
@@ -39,6 +26,16 @@ class TestCommandTableList < Test::Unit::TestCase
     table_list.each do |table|
       assert_equal(Groonga::Client::Response::TableList::Table, table.class)
     end
+
+    table = table_list[1]
+    assert_equal(256, table.id)
+    assert_equal("Lexicon", table.name)
+    assert_equal("/tmp/test.db.0000100", table.path)
+    assert_equal("TABLE_PAT_KEY|PERSISTENT", table.flags)
+    assert_equal("ShortText", table.domain)
+    assert_equal(nil, table.range)
+    assert_equal("TokenBigram", table.default_tokenizer)
+    assert_equal("NormalizerAuto", table.normalizer)
   end
 end
 
