756b62feffcef53b3d3511094b0826fdb8e6c295
Kouhei Sutou <kou@clear-code.com>
2013-09-13 12:26:18 +0900

    test: indent

--- test/response/test-select.rb
+++ test/response/test-select.rb
@@ -19,16 +19,16 @@ require "response/helper"
 
 class TestResponseSelect < Test::Unit::TestCase
   class TestParseRawResponse < self
-  include TestResponseHelper
+    include TestResponseHelper
 
-  def test_select
-    header = [0,1372430096.70991,0.000522851943969727]
-    body = [[[1], [["_id", "UInt32"]], [1]]]
-    raw_response = [header, body].to_json
+    def test_select
+      header = [0,1372430096.70991,0.000522851943969727]
+      body = [[[1], [["_id", "UInt32"]], [1]]]
+      raw_response = [header, body].to_json
 
-    response = parse_raw_response("select", raw_response)
-    assert_equal(Groonga::Client::Response::Select, response.class)
-  end
+      response = parse_raw_response("select", raw_response)
+      assert_equal(Groonga::Client::Response::Select, response.class)
+    end
   end
 
   class TestBody < self
