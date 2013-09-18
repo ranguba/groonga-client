6ac0c7dcc04ed65a05094e29f7f4b98bd6c0ea6c
Kouhei Sutou <kou@clear-code.com>
2013-09-18 17:47:37 +0900

    test: remove spaces arround "=" for default value

--- test/response/helper.rb
+++ test/response/helper.rb
@@ -3,7 +3,7 @@ module TestResponseHelper
     make_command(command_name).send(:parse_raw_response, raw_response)
   end
 
-  def make_command(command_name, parameters = {})
+  def make_command(command_name, parameters={})
     command_class = Groonga::Command.find(command_name)
     command = command_class.new(command_name, parameters)
     Groonga::Client::Command.new(command)
