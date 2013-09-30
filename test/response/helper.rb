f33d8899bcdaaebf3efd328321b683c4b17ada49
Kouhei Sutou <kou@clear-code.com>
2013-09-30 16:39:19 +0900

    test: use Groonga::Client::Response.parse directly

--- test/response/helper.rb
+++ test/response/helper.rb
@@ -1,11 +1,24 @@
+# Copyright (C) 2013  Kouhei Sutou <kou@clear-code.com>
+# Copyright (C) 2013  Kosuke Asami
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
 module TestResponseHelper
   def parse_raw_response(command_name, raw_response)
-    make_command(command_name).send(:parse_raw_response, raw_response)
-  end
-
-  def make_command(command_name, parameters={})
     command_class = Groonga::Command.find(command_name)
-    command = command_class.new(command_name, parameters)
-    Groonga::Client::Command.new(command)
+    command = command_class.new(command_name, {})
+    Groonga::Client::Response.parse(command, raw_response)
   end
 end
