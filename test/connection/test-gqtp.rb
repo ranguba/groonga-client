028c5534c54c2e4e497e81b2be09c7ff71d4ba85
Kouhei Sutou <kou@clear-code.com>
2014-01-07 15:41:00 +0900

    test gqtp: don't receive two requests on close

--- test/connection/test-gqtp.rb
+++ test/connection/test-gqtp.rb
@@ -70,16 +70,14 @@ class TestConnectionGQTP < Test::Unit::TestCase
 
   def process_client_close(client)
     response_body = [].to_json
-    2.times do
-      header = GQTP::Header.parse(client.read(GQTP::Header.size))
-      client.read(header.size)
+    header = GQTP::Header.parse(client.read(GQTP::Header.size))
+    client.read(header.size)
 
-      response_header = GQTP::Header.new
-      response_header.size = response_body.bytesize
+    response_header = GQTP::Header.new
+    response_header.size = response_body.bytesize
 
-      client.write(response_header.pack)
-      client.write(response_body)
-    end
+    client.write(response_header.pack)
+    client.write(response_body)
   end
 
   class TestConnected < self
