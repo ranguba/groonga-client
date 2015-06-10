eeaa761f7812dad2bd2e30ada34736a551d8b869
Kouhei Sutou <kou@clear-code.com>
2015-06-10 16:39:28 +0900

    Recover default.rb
    It's needed...

--- lib/groonga/client.rb
+++ lib/groonga/client.rb
@@ -17,6 +17,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
+require "groonga/client/default"
 require "groonga/client/command"
 require "groonga/client/empty-request"
 require "groonga/client/protocol/gqtp"
