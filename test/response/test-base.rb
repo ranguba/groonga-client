24f77654a902105246b01f253c8a4756fc195f35
Kouhei Sutou <kou@clear-code.com>
2016-05-30 10:17:23 +0900

    test: indent

--- test/response/test-base.rb
+++ test/response/test-base.rb
@@ -19,98 +19,98 @@ require "response/helper"
 class TestResponseBase < Test::Unit::TestCase
   class TestHeader < self
     class TestCommandVersion1 < self
-    class TestStatusCode < self
-      def test_have_header
-        header = [
-          -21,
-          1396012478.14975,
-          0.00050806999206543,
-        ]
-        response = Groonga::Client::Response::Base.new(nil, header, nil)
-        assert_equal(-21, response.status_code)
-      end
+      class TestStatusCode < self
+        def test_have_header
+          header = [
+            -21,
+            1396012478.14975,
+            0.00050806999206543,
+          ]
+          response = Groonga::Client::Response::Base.new(nil, header, nil)
+          assert_equal(-21, response.status_code)
+        end
 
-      def test_no_header
-        response = Groonga::Client::Response::Error.new(nil, nil, nil)
-        assert_equal(0, response.status_code)
-      end
-    end
-
-    class TestStartTime < self
-      def test_have_header
-        start_time = 1396012478.14975
-        header = [
-          -21,
-          start_time,
-          0.00050806999206543,
-        ]
-        response = Groonga::Client::Response::Base.new(nil, header, nil)
-        assert_equal(Time.at(start_time), response.start_time)
-      end
-
-      def test_no_header
-        response = Groonga::Client::Response::Error.new(nil, nil, nil)
-        assert_equal(Time.at(0), response.start_time)
-      end
-    end
-
-    class TestElapsedTime < self
-      def test_have_header
-        elapsed_time = 0.00050806999206543
-        header = [
-          -21,
-          1396012478.14975,
-          elapsed_time,
-        ]
-        response = Groonga::Client::Response::Base.new(nil, header, nil)
-        assert_equal(elapsed_time, response.elapsed_time)
-      end
-
-      def test_no_header
-        response = Groonga::Client::Response::Error.new(nil, nil, nil)
-        assert_equal(0.0, response.elapsed_time)
-      end
-    end
-
-    class TestErrorMessage < self
-      def test_have_header
-        error_message = "invalid argument"
-        header = [
-          -21,
-          1396012478.14975,
-          0.00050806999206543,
-          error_message,
-        ]
-        response = Groonga::Client::Response::Base.new(nil, header, nil)
-        assert_equal(error_message, response.error_message)
-      end
-
-      def test_no_header
-        response = Groonga::Client::Response::Error.new(nil, nil, nil)
-        assert_nil(response.error_message)
-      end
-    end
-
-    class TestSuccess < self
-      def test_have_header
-        header = [
-          -21,
-          1396012478.14975,
-          0.00050806999206543,
-        ]
-        response = Groonga::Client::Response::Base.new(nil, header, nil)
-        assert do
-          not response.success?
+        def test_no_header
+          response = Groonga::Client::Response::Error.new(nil, nil, nil)
+          assert_equal(0, response.status_code)
         end
       end
 
-      def test_no_header
-        response = Groonga::Client::Response::Error.new(nil, nil, nil)
-        assert do
-          response.success?
+      class TestStartTime < self
+        def test_have_header
+          start_time = 1396012478.14975
+          header = [
+            -21,
+            start_time,
+            0.00050806999206543,
+          ]
+          response = Groonga::Client::Response::Base.new(nil, header, nil)
+          assert_equal(Time.at(start_time), response.start_time)
+        end
+
+        def test_no_header
+          response = Groonga::Client::Response::Error.new(nil, nil, nil)
+          assert_equal(Time.at(0), response.start_time)
         end
       end
-    end
+
+      class TestElapsedTime < self
+        def test_have_header
+          elapsed_time = 0.00050806999206543
+          header = [
+            -21,
+            1396012478.14975,
+            elapsed_time,
+          ]
+          response = Groonga::Client::Response::Base.new(nil, header, nil)
+          assert_equal(elapsed_time, response.elapsed_time)
+        end
+
+        def test_no_header
+          response = Groonga::Client::Response::Error.new(nil, nil, nil)
+          assert_equal(0.0, response.elapsed_time)
+        end
+      end
+
+      class TestErrorMessage < self
+        def test_have_header
+          error_message = "invalid argument"
+          header = [
+            -21,
+            1396012478.14975,
+            0.00050806999206543,
+            error_message,
+          ]
+          response = Groonga::Client::Response::Base.new(nil, header, nil)
+          assert_equal(error_message, response.error_message)
+        end
+
+        def test_no_header
+          response = Groonga::Client::Response::Error.new(nil, nil, nil)
+          assert_nil(response.error_message)
+        end
+      end
+
+      class TestSuccess < self
+        def test_have_header
+          header = [
+            -21,
+            1396012478.14975,
+            0.00050806999206543,
+          ]
+          response = Groonga::Client::Response::Base.new(nil, header, nil)
+          assert do
+            not response.success?
+          end
+        end
+
+        def test_no_header
+          response = Groonga::Client::Response::Error.new(nil, nil, nil)
+          assert do
+            response.success?
+          end
+        end
+      end
     end
   end
 end
